6e2096adad41131f0b96862e47ac4621e08b5d0f
Theophilus-Charis <theophilusofori819@gmail.com>
2025-05-13 10:34:19 +0000

    Day two update

---
+++ ifStatement.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ifStatement {
+    public static void main(String[] args) {
+        // If statement = perform a block of code if its condition is true
+
+        Scanner input = new Scanner(System.in);
+
+        int age;
+        String name;
+        boolean isStudent;
+
+        System.out.print("Enter your name: ");
+        name = input.nextLine();
+
+        System.out.print("How old are you: ");
+        age = input.nextInt();
+
+        System.out.print("Are you a student (true/false): ");
+        isStudent = input.nextBoolean();
+
+        // Group One
+        if(name.isEmpty()){
+            System.out.println("You did not enter your name😒🥵");
+        }
+        else{
+            System.out.println("Hello "+name+"!😊");
+        }
+
+
+        
+
+        // Group Two
+        if(age>=18){
+            System.out.print("You are matured");
+        }
+        else if (age < 0 || age == 0) {
+            System.out.println("You have not being born yet");
+        }
+        else{
+            System.out.println("You are a child");
+        }
+
+        // Group Three
+        if (isStudent) {
+            System.out.println("You are a student");
+        } else {
+            System.out.println("You are not a student");
+        }
+
+        input.close();
+    }
+
+}
---
+++ randomNumbers.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class randomNumbers {
+    public static void main(String[] args) {
+        // working with Random numbers in Java
+
+        Random random = new Random();
+        
+        int number1;
+        int number2;
+        int number3;
+
+        number1 = random.nextInt(1,100);
+        number2 = random.nextInt(1,100);
+        number3 = random.nextInt(1,100);
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+        //BOOLEAN
+        boolean isHead;
+        isHead = random.nextBoolean();
+
+        if (isHead) {
+            System.out.println("Heads");
+        } else {
+            System.out.println("Tails");
+        }    
+    }
+
+}
